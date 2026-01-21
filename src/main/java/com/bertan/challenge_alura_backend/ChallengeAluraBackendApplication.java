90b00e7902c74547bdeb0e9bbc526a5b21f922eb
Roger Bertan <roger.bertan@gmail.com>
2026-01-21 12:26:46 -0300

    feat: initial commit

---
+++ src/main/java/com/bertan/challenge_alura_backend/ChallengeAluraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bertan.challenge_alura_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAluraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAluraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bertan/challenge_alura_backend/ChallengeAluraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertan.challenge_alura_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
